35adffc99474c0e6a027d32258c3300ec25e707c
nokib-baybit <nokib@baybit.com>
2024-01-10 13:21:52 +0600

    api updated

--- src/api.js
+++ src/api.js
@@ -1,4 +1,7 @@
-const BASE_URL = "https://admin.feerot.com/api";
-export const IMG_BASE_URL = "https://admin.feerot.com/public";
+// const BASE_URL = "https://admin.feerot.com/api";
+// export const IMG_BASE_URL = "https://admin.feerot.com/public";
+
+const BASE_URL = "http://16.171.181.94:90/api";
+export const IMG_BASE_URL = "http://16.171.181.94:90/public";
 
 export default BASE_URL;
